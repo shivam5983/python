41880d48da1a3ecca187bb1969198a80c8ccd9d9
shivam5983 <119227879+shivam5983@users.noreply.github.com>
2022-12-01 22:18:59 +0530

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,8 @@
+#Create a greeting for your Program.
+print("Welcome to the band name Generator.")
+#Ask the user for the city that they grew up.
+city=input("Which city did you grow up?\n")
+pet=input("What is the name of a pet?\n")
+print("the band name generator could be"+" "+ city +" "+ pet)
+
+
